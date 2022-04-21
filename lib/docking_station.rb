c6dc01a0d96d23d87d8e1f590b81c6025009a970
Anish <anishkakaiya@outlook.com>
2022-04-21 17:02:45 +0100

    refactor with private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,24 @@ class DockingStation
   end
   
   def release_bike
-    fail 'WRONG!!' if @bikes.empty?
+    fail 'WRONG!!' if empty?
     @bikes.pop
   end
 
   def dock_bike(bike)
-    fail "Error, dock station is full." if @bikes.count >= 20
+    fail "Error, dock station is full." if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    return true if @bikes.count >= 20
+    false
+  end
+
+  def empty?
+    return true if @bikes.empty?
+    false
   end
 end
